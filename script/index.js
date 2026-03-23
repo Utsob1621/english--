1ee21f75a65a290a0cb590a8668d48c196be61ab
Utsob1621 <utsobsaha9072@gmail.com>
2026-03-24 04:14:50 +0600

    added LessonButton fetch and display lessons using DocumentFragment for optimization

--- script/index.js
+++ script/index.js
@@ -29,3 +29,26 @@ const loginBtnContainer = () => {
   }
 };
 
+const loadLessonButton = async () => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/levels/all`,
+  );
+  const data = await response.json();
+  displayLesson(data.data);
+};
+
+const displayLesson = (lessons) => {
+  lessons.forEach((lesson) => {
+    const lessonsBtnsContainer = document.getElementById(
+      "lessons-btns-container",
+    );
+    const lessonDiv = document.createElement("div");
+    lessonDiv.innerHTML = `
+        <button onclick = loadWord('${lesson.level_no}') class = "btn text-[#00BCFF] hover:bg-[#00BCFF] hover:text-white border-2 border-[#00BCFF]">
+        <img src="./assets/fa-book-open.png" alt=""> Lesson-${lesson.level_no}</button>
+      `;
+    lessonsBtnsContainer.appendChild(lessonDiv);
+  });
+};
+
+
