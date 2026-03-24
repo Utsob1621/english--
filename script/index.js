a66b8700ff8640ae444854043181cf45213f0bd9
Utsob1621 <utsobsaha9072@gmail.com>
2026-03-24 10:37:18 +0600

    implement dynamic word loading and display logic

--- script/index.js
+++ script/index.js
@@ -52,3 +52,73 @@ const displayLesson = (lessons) => {
 };
 
 
+const loadWord = async (lessonId) => {
+  // hideLesson("word-container");
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/level/${lessonId}`,
+  );
+  const data = await response.json();
+
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  displayWord(data.data);
+};
+
+const displayWord = async (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    const wordDiv = document.createElement("div");
+    wordDiv.innerHTML = `
+  <div class="bg-white rounded-xl shadow-sm p-8 flex flex-col items-center text-center space-y-4 border border-gray-100 h-full">
+        <h2 class="text-3xl font-bold text-gray-800">${word.word}</h2>
+        
+        <div class="space-y-1">
+          <p class="text-gray-500 font-medium">Meaning /Pronunciation</p>
+          <p class="text-xl text-gray-700 font-semibold">"${word.meaning} / ${word.pronunciation}"</p>
+        </div>
+
+        <div class="flex justify-between w-full mt-6">
+          <button class="details p-3 bg-blue-50 text-blue-500 rounded-lg hover:bg-blue-100 transition-colors btn">
+            <img class = "w-4 h-4" src="./assets/info.png" alt="">
+          </button>
+          <button onclick="speak('${word.word}')" class="p-3 bg-blue-50 text-blue-500 rounded-lg hover:bg-blue-100 transition-colors btn">
+          <img class = "w-4 h-4" src="./assets/speaker-filled-audio-tool.png" alt=""></button>
+        </div>
+      </div>
+    `;
+    wordContainer.appendChild(wordDiv);
+  });
+};
+
+const loadDetails = async () => {
+  const response = await fetch(``);
+  const data = await response.json();
+  console.log(data);
+};
+
+const learnBtnContainer = () => {
+  const learnBtn = document.getElementById("learn");
+  const learnSection = document.getElementById("learn-section");
+
+  if (learnBtn && learnSection) {
+    learnBtn.addEventListener("click", () => {
+      learnSection.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+};
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  loginBtnContainer();
+  logoutBtnContainer();
+  learnBtnContainer();
+  fcqBtnContainer();
+  loadLessonButton();
+});
